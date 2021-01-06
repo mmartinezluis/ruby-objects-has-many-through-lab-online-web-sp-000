6b70e3f8bd13ced0c2099e415ebad0202b1ef94a
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 06:38:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,7 +12,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|appoinrment| appointment.patient == self}
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def self.all
