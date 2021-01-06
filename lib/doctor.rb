705841406fa533942a0e9debd4137f5a64943b8a
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 06:20:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,8 +16,8 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
-  def new_appointment(date, genre)
-    Appointment.new(date, self, genre)
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
   end
 
   def patients
