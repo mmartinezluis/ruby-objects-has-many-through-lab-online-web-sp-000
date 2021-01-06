bb69ca30a869e3c9f25223b6673c7e8f019a4a3e
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 06:15:02 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+
+class Appointment
+  attr_accessor :name, :artist, :genre
+  @@all = [ ]
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
