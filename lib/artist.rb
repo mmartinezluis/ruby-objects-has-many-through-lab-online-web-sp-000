da1fe1cca34a05bb6abd2e22643d6c14f24521a9
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 05:46:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,4 +12,11 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def new_song
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    songs.collect {|this_artist_song| this_artist_song.genre}
+  end
+  
