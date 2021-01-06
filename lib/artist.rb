863268c2d22ecb3ff233cfa78585aa8e8f26b0d5
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 05:42:09 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+
+class Artist
+  attr_accessor :name
+  @@all = [ ]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def new_song
